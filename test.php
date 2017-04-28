7dc19860f1195eda663fb129ff2dd37ffb49e81f
Chen Hao <276073970@qq.com>
2017-04-28 22:53:51 +0800

    First commit from Chen Hao

---
+++ test.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>A page for test</title>
+  </head>
+  <body>
+    <h1>This just a page for test!</h1>
+    <p>Hi guys! We can do any test in this page, but please don't change the index page if it's not necessary!</p>
+  </body>
+</html>
