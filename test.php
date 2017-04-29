6201d90f21b2c148b4f2570f9f8750eb98569570
Chen Hao <276073970@qq.com>
2017-04-29 22:47:59 +0800

    I write something for Aman

--- test.php
+++ test.php
@@ -7,5 +7,6 @@
   <body>
     <h1>This just a page for test!</h1>
     <p>Hi guys! We can do any test in this page, but please don't change the index page if it's not necessary!</p>
+    <p>A Man! What are you doing? Come on!</p>
   </body>
 </html>
